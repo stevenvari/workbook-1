803c9a47577cdfac22a7b606fdaad92fc220dc67
Steven Vargas <stevenvari13@gmail.com>
2025-04-10 11:21:54 -0400

    complete ShoppingList app

---
+++ ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("soap");
+        System.out.println("fruits");
+        System.out.println("brush");
+        System.out.println("apple");
+        System.out.println("plum");
+        System.out.println("kiwi");
+        System.out.println("beans");
+        System.out.println("rice");
+        System.out.println("juice");
+        System.out.println("shoes");
+    }
+}
