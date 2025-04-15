d8a2311981cffa5e4275649cc9776dec66ded3a2
Steven Vargas <stevenvari13@gmail.com>
2025-04-15 14:06:57 -0400

    complete MethodExercise

---
+++ payrollCalculator/src/main/java/com/Pluralsight/MethodExercise.java
@@ -0,0 +1,28 @@
+package com.Pluralsight;
+
+public class MethodExercise {
+    public static void main(String[] args) {
+        brushTeeth();
+
+        String movie = favoriteMovie();
+        System.out.println("My favorite movie is: " + movie);
+
+        int sum = addTwoNumbers( 600, 100);
+        System.out.println("The sum of two numbers is: " + sum);
+
+    }
+
+    public static void brushTeeth() {
+        System.out.println("Steps to brush your teeth: ");
+        System.out.println("Step 1");
+        System.out.println("Step 2");
+        System.out.println("Step 3");
+
+    }
+    public static String favoriteMovie() {
+        return  "Titanic";
+    }
+    public static  int addTwoNumbers(int a, int b) {
+        return a + b;
+    }
+}
